f2ed7a201dc5257899ba92fe08d6b86dce39d8a9
khoikhoi2901 <139947224+khoikhoi2901@users.noreply.github.com>
2023-07-19 13:22:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Do you ",
+    introTitle: 'Lâm Diễm Huỳnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
